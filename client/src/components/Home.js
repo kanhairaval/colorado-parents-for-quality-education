0a39c8120f1b0d9133e5658e45a966e7c1a0cd69
Kanhai Raval <kanhairaval@gmail.com>
2024-03-17 20:54:34 -0400

    more homepage content added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,8 +7,14 @@ const Home = () => {
         <section className = "home-section">
             <img className = "home-bg-image" alt = "family-doing-homework" src = "family-doing-homework-img.jpg"/>
             <div className="overlay-text">Colorado Parents for Quality Education - Informed Parents, Thriving Children</div>
-            <h1>Welcome to CPQE™</h1>
-            <h3></h3>
+            <div className="button-container">
+                <button className = "homepage-buttons">Donate</button>
+                <h1 className = "home-heading">Welcome to Colorado Parents for Quality Education</h1>
+                <h3>Founded in 2022, CPQE™ is more than an organization—it's a promise of opportunity. We envision a Colorado where every child, no matter their circumstances, has the chance to succeed. We work tirelessly to bridge the gap, offering support, resources, and a sense of community to families across Colorado Springs.</h3>
+                <h1 className = "home-heading">The Issue</h1>
+                <h3>Education should be a basic human right for every person, not a luxury for the few.</h3>
+                <button className = "homepage-buttons">Learn More</button>
+            </div>
         </section>
     )
 };
