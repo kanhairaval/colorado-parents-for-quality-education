53e04f4dad0f86af86761c43bc6d74effa6e8569
Kanhai Raval <kanhairaval@gmail.com>
2024-04-18 21:54:17 -0400

    regular navbar looks fine, will start working on burger option now

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-scroll";
 import "../style/NavBar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -9,66 +10,68 @@ const NavBar = () => {
     setIsActive(!isActive);
   };
 
+  const closeMenu = () => {
+    setIsActive(false);
+  };
+
   return (
     <nav className={`navbar ${isActive ? "active" : ""}`}>
-      <div className="navbar-logo">
+      <div className="logo">
         <img src="/logo.png" alt="Logo" />
       </div>
-      <ul className={`navbar-links ${isActive ? "active" : ""}`}>
-        <li className="nav-item">
-          <Link
-            to="home"
-            smooth={true}
-            duration={500}
-            spy={true}
-            offset={-70}
-            onClick={() => setIsActive(false)}
-          >
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="get-involved"
-            smooth={true}
-            duration={500}
-            spy={true}
-            offset={-70}
-            onClick={() => setIsActive(false)}
-          >
-            Get Involved
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="donate"
-            smooth={true}
-            duration={500}
-            spy={true}
-            offset={-70}
-            onClick={() => setIsActive(false)}
-          >
-            Donate
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="about"
-            smooth={true}
-            duration={500}
-            spy={true}
-            offset={-70}
-            onClick={() => setIsActive(false)}
-          >
-            About Us
-          </Link>
-        </li>
-      </ul>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/get-involved" onClick={closeMenu}>
+              Get Involved
+            </Link>
+          </li>
+          <li>
+            <Link to="/donate" onClick={closeMenu}>
+              Donate
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us" onClick={closeMenu}>
+              About Us
+            </Link>
+          </li>
+        </ul>
+      </div>
       <button className="navbar-toggle" onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
+        {isActive ? <FaTimes /> : <FaBars />}
       </button>
+      {isActive && (
+        <div className="burger-menu">
+          <ul>
+            <li>
+              <Link to="/" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/get-involved" onClick={closeMenu}>
+                Get Involved
+              </Link>
+            </li>
+            <li>
+              <Link to="/donate" onClick={closeMenu}>
+                Donate
+              </Link>
+            </li>
+            <li>
+              <Link to="/about-us" onClick={closeMenu}>
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
